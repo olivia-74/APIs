0dcdc6ba35f360030933ef53c6a9330a5f22c018
olivia <oli.dresch@gmail.com>
2023-08-22 12:25:23 -0300

    segunda API

--- src/server.js
+++ src/server.js
@@ -2,7 +2,24 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.get('/api/alunos', (request, response) => {
-    response.send('happy happy happy')
+    // response.send('happy happy happy');
+    response.status(200).json({
+        status: 'success',
+        data: { //dados do banco de dados
+            alunos: [  // array de objetos
+                {
+                    id_use: 1,
+                    nome: 'olivia',
+                    idade: 19,
+                },
+                {
+                    id_use: 2,
+                    nome: 'leonardo',
+                    idade: 19,
+                },
+            ]
+        }
+    })
 })
 const PORT = 3006;
 app.listen(PORT, () => console.log(`running at port ${PORT}`))
