9ccf7387d5646229cea53a20690219ce7ff71e23
olivia <oli.dresch@gmail.com>
2023-08-22 12:07:19 -0300

    primeira API

--- server.js
+++ server.js
@@ -2,3 +2,5 @@ const express = require('express'); // pacote express faz as requisições ao ba
 
 const app = express() // cria a instancia na variavel app, express: variavel no servidor app
 app.listen(3000, () => console.log('running at port 3000'))// funcao listen: ver se o servir esta funcionando, "ouve as entradas e saidas"
+
+
