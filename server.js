55d3c8a63d5d2bf5e5b11477d35719518280aed9
Olivia Dresch <84184891+olivia-74@users.noreply.github.com>
2023-09-04 12:09:38 -0300

    Delete server.js

--- server.js
+++
@@ -1,4 +0,0 @@
-const express = require('express'); // pacote express faz as requisições ao banco de dados e retorna as informacoes
-
-const app = express() // cria a instancia na variavel app, express: variavel no servidor app
-app.listen(3000, () => console.log('running at port 3000'))// funcao listen: ver se o servir esta funcionando, "ouve as entradas e saidas"
